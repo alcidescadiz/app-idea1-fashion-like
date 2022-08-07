5334b7947a029e000e430523b9e8cbc3e9c49750
alcidescadiz <alcides.cadiz@gmail.com>
2022-08-06 21:58:50 -0400

    update

--- src/public/components/gallery/gallery.js
+++ src/public/components/gallery/gallery.js
@@ -1,72 +1,47 @@
-import { useState } from "../../modules/myUseSte.js"
+import { useGenerateGalleryCard } from "./GalleryGenerator.js";
+let { componentTable, inicialArrayObjects, RenderTable, setObjectInArray } =
+  useGenerateGalleryCard();
 
-export function Gallery([fn]) {
-    let [gallery, setGallery] = useState('')
-    let {status, like, dislike} = fn()
-    if(!status) window.location.hash= '#login'
+//-- 1ro exportar componente
+export { componentTable as Gallery };
 
-    let div = document.createElement('div')
-    div.id = 'div-gallery-app'
-    let post = ``
-    if(!status) window.location.hash= '#login'
-    function generateGallery(like, dislike){
-        fetch(window.location.origin+'/v1-api-post')
-             .then(res => res.json())
-             .then(json => {
-                json.posts.map(e=>{
-                    let templateLike= `
-                    <div class="position-absolute top-0 end-0 m-2">
-                        <span class="badge rounded-pill text-bg-success">Favorite</span>
-                    </div>`
-                    let favorite= like.includes(e.id)? templateLike : ''
-                    let templateDislike= `
-                    <div class="position-absolute top-0 end-0 m-2">
-                        <span class="badge rounded-pill text-bg-danger">Disapprove</span>
-                    </div>`
-                    let disgusting= dislike.includes(e.id)? templateDislike : ''
-                    post += `
-                    <div class="card m-3" style="max-width: 100%;">
-                      <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src=${e.img} class="img-fluid rounded-start w-100" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="position-relative bottom-0 start-0 m-2">
-                                <span class="badge rounded-pill text-bg-secondary">${e.date}</span>
-                            </div>
-                            <div class="card-body">
-                                ${favorite}
-                                ${disgusting}
-                            <h5 class="card-title">${e.post}</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                            <br>
-                            <br>
-                            <div class="position-absolute bottom-0 end-0 m-2">
-                                <button name=${e.id} class="btn btn-primary me-md-2" type="button">Like</button>
-                                <button name=${e.id} class="btn btn-warning" type="button">Dislike</button>
-                            </div>
-                        </div>
-                      </div>
-                    </div>
-                    `
-            })
-                setGallery(`
-                    <div class='container pb-5'>
-                        <div class="g-2 pb-4">
-                            ${post}
-                        </div> 
-                    </div>
-                `)
-                
-                div.innerHTML = gallery()
-            })
-    }
-    generateGallery(like, dislike)
-    
- 
-    return div
-}
+/**
+ * -- 3ro
+ * Estado inicial dela base de datos-- USAR ESTA PARA EL FECTH
+ */
+
+fetch(window.location.origin + "/v1-api-post", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((res) => res.json())
+  .then((json) => {
+    console.log("render galeria");
+    inicialArrayObjects(json.posts);
+  });
 
 
+/**
+ * Acciones crud:
+ *   setArrayObjects({ value: getData(), mode: "add" })    // add
+ *   setArrayObjects({ value: getData(), mode: "update" }) // update
+ *   setArrayObjects({ mode: "delete", id });              // delete
+ */
 
+//-- crear eventos de --like y dislike--EJEMPLOS
+
+document.addEventListener("click", (e) => {
+  //--- eliminar - delete ---
+  if (e.target.innerHTML === "Delete") {
+    /**
+     * logica de delete de base de datos
+     */
+    //if(confirm('Desea eliminar datos?')){
+    let id = Number(e.target.parentNode.parentNode.childNodes[0].innerHTML);
+    setObjectInArray({ mode: "delete", id });
+    RenderTable();
+    //}
+  }
+});
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,10 +7,7 @@ Route({                    template: Menu,        props: [Login],  protect: fals
 Route({ path: "",          template: Gallery,     props: [Login],  protect: false  });
 Route({ path: "#login",    template: login,       props: [Login],  protect: false  });
 Route({ path: "#register", template: Register,                     protect: false  });
-Route({ path: "#admin",    template: Admin,                        protect: true   });
-// Route({ path: "#inputs", template: componentTable,              protect: false  });
-// Route({ path: "#login",  template: htmlLogin,   props: [Login], protect: false  });
-// Route({ path: "#logOut", template: htmlLogOut,  props: [Login], protect: true   });
+Route({ path: "#admin",    template: Admin,                        protect: true  });
 Route({                    template: Foot,                         protect: false  });
 
 Render();
@@ -25,27 +22,27 @@ window.addEventListener('DOMContentLoaded', ()=>{
     console.log(text)
 }, {once:true})
 
-document.addEventListener("click", (e) => {
-    if(e.target.innerHTML === "Like" || e.target.innerHTML === "Dislike"){
-      fetch(window.location.origin+'/v1-api-users/favorite',
-          {
-              body: JSON.stringify({
-                data: e.target.name,
-                likeDislike: e.target.innerHTML.toLowerCase()
-              }),
-              method:'POST',
-              headers:{
-                  'Content-Type': 'application/json'
-              }
-          }
-      ).then(res => res.json())
-        .then(json => {
-          if(json.user){
-            Login({ status:true, name: json.user.name, like:json.user.like,  dislike:json.user.dislike })
-            Render();
-            console.log('render like o dislike')
-          }
-      })
-    }
+// document.addEventListener("click", (e) => {
+//     if(e.target.innerHTML === "Like" || e.target.innerHTML === "Dislike"){
+//       fetch(window.location.origin+'/v1-api-users/favorite',
+//           {
+//               body: JSON.stringify({
+//                 data: e.target.name,
+//                 likeDislike: e.target.innerHTML.toLowerCase()
+//               }),
+//               method:'POST',
+//               headers:{
+//                   'Content-Type': 'application/json'
+//               }
+//           }
+//       ).then(res => res.json())
+//         .then(json => {
+//           if(json.user){
+//             Login({ status:true, name: json.user.name, like:json.user.like,  dislike:json.user.dislike })
+//             Render();
+//             console.log('render like o dislike')
+//           }
+//       })
+//     }
 
-  });
+//   });
